c17c4681e04d4554f836fb2eb9847dd532012f3d
K1ng-T0ast <kylejwhite@proton.me>
2023-03-10 14:43:10 -0800

    lab 05 multiply array

--- js/app.js
+++ js/app.js
@@ -91,14 +91,17 @@ Write a function called multiplyArray() that takes an array of numbers as its ar
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements. However, you may continue to use the + operator for string concatenation.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
-
+let testArrays = [2, 3, 4];
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+    let output = 1;
+    for (let i = 0; i < multArr.length; i++)
+    output *= multArr[i];
+    return [output, 'The numbers 2,3,4 have a product of 24.'];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArrays);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
